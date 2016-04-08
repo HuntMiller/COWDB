fe40a121dc2b92c85d3cf322d1ca55d07c305c2a
devinwelch <devinmwelch@gmail.com>
2016-04-07 22:05:02 -0400

    Set up a RequestQueue to read in the JSON object give in Brian's url
    Temporarily added a TextView to view the response is correct
    
    Modified background

--- app/src/main/java/ut/seniordesign/cowdb/MainActivity.java
+++ app/src/main/java/ut/seniordesign/cowdb/MainActivity.java
@@ -7,6 +7,15 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
@@ -24,6 +33,32 @@ public class MainActivity extends AppCompatActivity {
         CustomAdapter adapter = new CustomAdapter(list, this);
         listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
+
+        //make the request
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://cowdb.kazeinc.com/endpoints";
+
+        String testing = "";
+        JsonObjectRequest jsObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                //do something with the response
+                //textView in place solely to see that the request went through
+                textView.setText(response.toString());
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //do something on error
+            }
+        });
+
+        MySingleton.getInstance(this).addToRequestQueue(jsObjectRequest);
 
         Random rand = new Random();
         generateButtons(rand.nextInt(10) + 1);
